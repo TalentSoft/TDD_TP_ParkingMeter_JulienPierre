db51f8676b608e6605f5a7ad559ea48ec44b7c8b
Julien Plée <julien.plee@live.Fr>
2013-07-05 10:48:03 +0200

    Résultat de 50 minutes de Peer Programming

---
+++ Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ParcMetre
+{
+    [TestFixture]
+    public class TestParcMeter
+    {
+        [Test]
+        public void ParcMeter_InsertCoins_InsertOneEuro_GetTwentyMinutes()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(1);
+
+            Assert.AreEqual(20, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertCoins_InsertThreeEuro_GetTwoHours()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(3);
+
+            Assert.AreEqual(60 * 2, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertCoins_InsertTwoEuro_GetOneHour()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(2);
+
+            Assert.AreEqual(60, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertCoins_InsertFiveEuro_GetHalfDay()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(5);
+
+            Assert.AreEqual(12 * 60, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertCoins_InsertHeightEuro_GetDay()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(8);
+
+            Assert.AreEqual(24 * 60, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertCoins_InsertTenEuro_GetOneDayAndOneHour()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(10);
+
+            Assert.AreEqual(24 * 60 + 60, parcMeter.GetTime());
+        }
+
+        [Test]
+        public void ParcMeter_InsertThreeEuroAt8_GetParckingTo10()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(3);
+
+            var now = new DateTime(2012, 1, 15, 8, 0, 0);
+            Assert.AreEqual(now.AddHours(2), parcMeter.GetEndDate(now));
+        }
+
+        [Test]
+        public void ParcMeter_InsertThreeEuroAt11_GetParckingTo15()
+        {
+            ParcMeter parcMeter = new ParcMeter();
+
+            parcMeter.InsertCoins(3);
+
+            var now = new DateTime(2012, 1, 15, 11, 0, 0);
+            Assert.AreEqual(now.AddHours(4), parcMeter.GetEndDate(now));
+        }
+    }
+}
---
+++ ParcMeter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParcMetre
+{
+    class ParcMeter
+    {
+        int ValeurCourante { get; set; }
+
+        internal void InsertCoins(int p)
+        {
+            ValeurCourante = p;
+        }
+
+        internal int GetTime()
+        {
+            int timeForOneEuro = 20;
+            int timeForTwoEuro = 60;
+            int timeForThreeEuro = 60 * 2;
+            int timeForFiveEuro = 60 * 12;
+            int timeForEightEuro = 60 * 24;
+
+            int restOfMoney = ValeurCourante;
+
+            int timeEightEuro = (int)Math.Floor(restOfMoney / 8d);
+            restOfMoney = restOfMoney - timeEightEuro * 8;
+            int timeFiveEuro = (int)Math.Floor(restOfMoney / 5d);
+            restOfMoney = restOfMoney - timeFiveEuro * 5;
+            int timeThreeEuro = (int)Math.Floor(restOfMoney / 3d);
+            restOfMoney = restOfMoney - timeThreeEuro * 3;
+            int timeTwoEuro = (int)Math.Floor(restOfMoney / 2d);
+            restOfMoney = restOfMoney - timeTwoEuro * 2;
+            int timeOneEuro = (int)Math.Floor(restOfMoney / 1d);
+
+            return timeEightEuro*timeForEightEuro + timeFiveEuro*timeForFiveEuro + timeThreeEuro*timeForThreeEuro +
+                   timeTwoEuro*timeForTwoEuro + timeOneEuro*timeForOneEuro;
+        }
+
+        internal DateTime GetEndDate(DateTime lastInsertedCoinDate)
+        {
+            int startfreeTimeMidi = 12;
+            int endfreeTimeMidi = 14;
+
+            if (lastInsertedCoinDate.Hour < startfreeTimeMidi)
+            {
+                var calculatedDate = lastInsertedCoinDate.AddMinutes(GetTime());
+
+                if (calculatedDate.Hour < startfreeTimeMidi)
+                    return calculatedDate;
+                else
+                {
+                    return calculatedDate.AddHours(endfreeTimeMidi - startfreeTimeMidi);
+                }
+            }
+            
+            return lastInsertedCoinDate.AddMinutes(GetTime());
+        }
+    }
+}
